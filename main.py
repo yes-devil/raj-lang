b8d013b818f20cb53d7c4cb2a09f004618017a1c
raja <263445693+yes-devil@users.noreply.github.com>
2026-09-07 09:53:12 +0530

    add ask()

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import os
 source_file = sys.argv[1]
 target_file = "output.cpp"
 
-if not(source_file[-3:]==".my"):
+if not(source_file[-4:]==".raj"):
     print("Not a .my file")
+    exit(0)
 
-def convertIt(line):
+def _say(line):
     match = re.match(r'\s*say\((.*)\)\s*', line)
 
     if not match:
@@ -28,12 +29,36 @@ def convertIt(line):
     return '\t' + 'cout << ' + ' << '.join(p.strip() for p in parts) + ';' + '\n'
 
 
+def _ask(line):
+    match = re.match(r'\s*ask\((.*)\)\s*', line)
+
+    if not match:
+        return line
+
+    args = match.group(1)
+
+    args = re.sub(
+        r'sum\(\s*([^,]+)\s*,\s*([^)]+)\)',
+        r'(\1 + \2)',
+        args
+    )
+
+    parts = re.findall(r'"[^"]*"|\'[^\']*\'|[^;;]+', args)
+
+    return '\t' + 'cin >> ' + ' >> '.join(p.strip() for p in parts) + ';' + '\n'
+
+
 with open(source_file, "r") as sf, open(target_file, "w") as tf:
     tf.write("#include<iostream>\n")
     tf.write("using namespace std;\n")
     for line in sf:
         if "  say(" in line:
-            result = convertIt(line)
+            result = _say(line)
+            # print(result)
+            tf.write(result)
+            # tf.write("\tline with say\n")
+        elif "  ask(" in line:
+            result = _ask(line)
             # print(result)
             tf.write(result)
             # tf.write("\tline with say\n")
@@ -42,5 +67,5 @@ with open(source_file, "r") as sf, open(target_file, "w") as tf:
 
 
 subprocess.run(['g++', 'output.cpp', '-o', 'output'])
-# os.remove('output.cpp')
+os.remove('output.cpp')
 subprocess.run(['./output'])

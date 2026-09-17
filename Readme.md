c56518ac4513fe85eb1e1c5b208af51eaee5a0c0
raja <263445693+yes-devil@users.noreply.github.com>
2026-09-17 21:52:16 +0530

    structure the project repo well for good looking code base. Changed the source codes form main.py to raj.py

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This repo is hosts the source codes of "raj" programming language. 
+"Raj" is a general purpose programming language that is built over the "C++" language to make the "C++" developmet smoother.
+```main.raj" is just for trial.
---
+++ raj.py
@@ -0,0 +1,94 @@
+import sys
+import re
+import subprocess
+import os
+
+source_file = sys.argv[1]
+target_file = "output.cpp"
+
+if not(source_file[-4:]==".raj"):
+    print("Not a .raj file")
+    exit(0)
+
+def _say(line):
+    match = re.match(r'\s*say\((.*)\)\s*', line)
+
+    if not match:
+        return line
+
+    args = match.group(1)
+
+    args = re.sub(
+        r'sum\(\s*([^,]+)\s*,\s*([^)]+)\)',
+        r'(\1 + \2)',
+        args
+    )
+
+    parts = re.findall(r'"[^"]*"|\'[^\']*\'|[^;;]+', args)
+
+    return '\t' + 'cout << ' + ' << '.join(p.strip() for p in parts) + ';' + '\n'
+
+def _sayln(line):
+    match = re.match(r'\s*sayln\((.*)\)\s*', line)
+
+    if not match:
+        return line
+
+    args = match.group(1)
+    if len(args) == 0:
+        return '\t' + 'cout << endl;' + '\n'
+
+    args = re.sub(
+        r'sum\(\s*([^,]+)\s*,\s*([^)]+)\)',
+        r'(\1 + \2)',
+        args
+    )
+
+    parts = re.findall(r'"[^"]*"|\'[^\']*\'|[^;;]+', args)
+
+    return '\t' + 'cout << ' + ' << '.join(p.strip() for p in parts) + ' << endl;' + '\n'
+
+
+def _ask(line):
+    match = re.match(r'\s*ask\((.*)\)\s*', line)
+
+    if not match:
+        return line
+
+    args = match.group(1)
+
+    args = re.sub(
+        r'sum\(\s*([^,]+)\s*,\s*([^)]+)\)',
+        r'(\1 + \2)',
+        args
+    )
+
+    parts = re.findall(r'"[^"]*"|\'[^\']*\'|[^;;]+', args)
+
+    return '\t' + 'cin >> ' + ' >> '.join(p.strip() for p in parts) + ';' + '\n'
+
+
+with open(source_file, "r") as sf, open(target_file, "w") as tf:
+    tf.write("#include<iostream>\n")
+    tf.write("using namespace std;\n")
+    for line in sf:
+        if "   say(" in line:
+            result = _say(line)
+            # print(result)
+            tf.write(result)
+            # tf.write("\tline with say\n")
+        elif "   ask(" in line:
+            result = _ask(line)
+            # print(result)
+            tf.write(result)
+            # tf.write("\tline with say\n")
+        elif "   sayln(" in line:
+            result = _sayln(line)
+            tf.write(result)
+        else:
+            tf.write(line)
+
+
+subprocess.run(['g++', 'output.cpp', '-o', 'output'])
+os.remove('output.cpp')
+subprocess.run(['./output'])
